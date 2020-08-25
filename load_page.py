0c7457999af606236aa93fbf9416df6ff27a7260
jdpalaciob <jdpalaciob@gmail.com>
2020-08-25 15:17:17 -0500

    Se redefine el request dentro de una funcion con la URL como parametro.
    La funcion regresa un objeto que contiene el codigo html, embellecido con beatiful soup.

--- load_page.py
+++ load_page.py
@@ -1,10 +1,15 @@
-""" Loading the webpage """
+""" Creating an object containing the webpage html code """
 
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get('https://www.fincaraiz.com.co/apartamentos/arriendo/medellin/')
-content = page.content
+def page_request(url):
+    """ Creates an object with the HTML code """
+    page = requests.get(url).content
 
-soup = BeautifulSoup(content, 'html.parser')
-print(soup.prettify())
+    return BeautifulSoup(page, 'html.parser')
+
+if __name__ == '__main__':
+
+    URL = 'https://www.fincaraiz.com.co/apartamentos/arriendo/medellin/'
+    print(page_request(URL))
